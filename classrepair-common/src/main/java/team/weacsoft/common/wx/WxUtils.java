e4da5a432dd27f9cb0b3f2d55d02f26dd48076ac
GreenHatHG <1239776759@qq.com>
2020-01-09 19:40:21 +0800

    修复配置文件读取问题

--- classrepair-common/src/main/java/team/weacsoft/common/wx/WxUtils.java
+++ classrepair-common/src/main/java/team/weacsoft/common/wx/WxUtils.java
@@ -1,38 +1,41 @@
 package team.weacsoft.common.wx;
 
-import cn.hutool.core.util.IdUtil;
 import cn.hutool.json.JSONObject;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import cn.hutool.json.JSONUtil;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import team.weacsoft.common.exception.BadRequestException;
 
 /**
  * @author GreenHatHG
  **/
 
+@Getter
+@Setter
 @Component
-@Data
 @ConfigurationProperties(prefix = "wx.utils")
 public class WxUtils {
+
     private String appid;
 
-    private String secret;
+    private String appsecret;
 
     private String grant_type;
 
-//    public JSONObject code2Session(String jsCode){
-//        JSONObject code2sessionResp =  WxRequests.wxAuth(jsCode, APPID, SECRET, GRANT_TYPE);
-//        if(code2sessionResp.getInt("errcode") != null){
-//            throw new Code2SessionException(JSONUtil.toJsonStr(code2sessionResp));
-//        }
-//        return code2sessionResp;
-//    }
-
     public JSONObject code2Session(String jsCode){
-        JSONObject code2sessionResp = new JSONObject();
-        code2sessionResp.put("openid", IdUtil.simpleUUID());
-        code2sessionResp.put("session_key", IdUtil.simpleUUID());
+        JSONObject code2sessionResp =  WxRequests.wxAuth(jsCode, appid, appsecret, grant_type);
+        if(code2sessionResp.getInt("errcode") != null){
+            throw new BadRequestException(444, JSONUtil.toJsonStr(code2sessionResp));
+        }
         return code2sessionResp;
     }
+
+//    public JSONObject code2Session(String jsCode){
+//        JSONObject code2sessionResp = new JSONObject();
+//        code2sessionResp.put("openid", IdUtil.simpleUUID());
+//        code2sessionResp.put("session_key", IdUtil.simpleUUID());
+//        return code2sessionResp;
+//    }
 }
