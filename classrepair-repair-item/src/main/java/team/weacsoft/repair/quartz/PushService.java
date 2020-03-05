c06c389e40380101ff2af45113d13cf9b86bfb34
GreenHatHG <1239776759>
2020-03-05 21:55:52 +0800

    添加紧急订单提醒

--- classrepair-repair-item/src/main/java/team/weacsoft/repair/quartz/PushService.java
+++ classrepair-repair-item/src/main/java/team/weacsoft/repair/quartz/PushService.java
@@ -1,5 +1,6 @@
 package team.weacsoft.repair.quartz;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.crypto.Mode;
 import cn.hutool.crypto.Padding;
@@ -12,7 +13,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import team.weacsoft.common.consts.RepairItemStateEnum;
 import team.weacsoft.common.wx.WxMaConfiguration;
+import team.weacsoft.qa.entity.QaType;
+import team.weacsoft.qa.service.impl.QaTypeServiceImpl;
 import team.weacsoft.repair.entity.PushInfo;
+import team.weacsoft.repair.entity.RepairItem;
 import team.weacsoft.repair.service.impl.RepairItemStateServiceImpl;
 import team.weacsoft.timetable.entity.DutyUserInfo;
 import team.weacsoft.timetable.service.impl.TimeTableServiceImpl;
@@ -35,6 +39,7 @@ public class PushService{
     private RepairItemStateServiceImpl repairItemService;
     private TimeTableServiceImpl timeTableService;
     private UserInfoServiceImpl userInfoService;
+    private QaTypeServiceImpl qaTypeService;
 
     @Value("${classrepair.push.key}")
     private String key;
@@ -47,11 +52,12 @@ public class PushService{
     private AES aes;
 
     @Autowired
-    public PushService(RestTemplate restTemplate, RepairItemStateServiceImpl repairItemService, TimeTableServiceImpl timeTableService, UserInfoServiceImpl userInfoService) {
+    public PushService(RestTemplate restTemplate, RepairItemStateServiceImpl repairItemService, TimeTableServiceImpl timeTableService, UserInfoServiceImpl userInfoService, QaTypeServiceImpl qaTypeService) {
         this.restTemplate = restTemplate;
         this.repairItemService = repairItemService;
         this.timeTableService = timeTableService;
         this.userInfoService = userInfoService;
+        this.qaTypeService = qaTypeService;
     }
 
     @PostConstruct
@@ -59,13 +65,33 @@ public class PushService{
         this.aes = new AES(Mode.CBC, Padding.PKCS5Padding, key.getBytes(), iv.getBytes());
     }
 
-    public void sendMessage(){
-       List<PushInfo> pushInfos = repairItemService.getBaseMapper().pushDao();
+    //发送定时事件信息
+    public void sendCronMessage(){
+        sendMessage(repairItemService.getBaseMapper().pushDao(), "请尽快处理");
+    }
+
+    //发送紧急订单信息
+    public void sendUrgentMessage(RepairItem repairItem){
+        PushInfo pushInfo = new PushInfo();
+        BeanUtil.copyProperties(repairItem, pushInfo);
+
+        UserInfo userInfo = userInfoService.getById(repairItem.getOrderer());
+        QaType qaType = qaTypeService.getById(repairItem.getEquipmentType());
+
+        pushInfo.setOrdererName(userInfo.getName());
+        pushInfo.setTitle(qaType.getTitle());
+
+        List<PushInfo> data = new ArrayList<>();
+        data.add(pushInfo);
+        sendMessage(data, "!!紧急订单，请马上处理!!");
+    }
+
+    public void sendMessage(List<PushInfo> pushInfos, String remark){
        List<DutyUserInfo> dutyUserInfos = timeTableService.getBaseMapper().getDutyUserInfos();
        if(dutyUserInfos != null && !dutyUserInfos.isEmpty()){
            for(PushInfo pushInfo : pushInfos){
                for(DutyUserInfo dutyUserInfo : dutyUserInfos){
-                   request(pushInfo, dutyUserInfo.getIdentityId());
+                   request(pushInfo, dutyUserInfo.getIdentityId(), remark);
                }
            }
        //没有在线维护人员，发给负责人
@@ -73,13 +99,13 @@ public class PushService{
            List<UserInfo> principals = userInfoService.getPrincipals();
            for(PushInfo pushInfo : pushInfos){
                for(UserInfo userInfo : principals){
-                   request(pushInfo, userInfo.getIdentityId());
+                   request(pushInfo, userInfo.getIdentityId(), remark);
                }
            }
        }
     }
 
-    public void request(PushInfo pushInfo, Long identityId){
+    public void request(PushInfo pushInfo, Long identityId, String remark){
         Map<String, Object> json = new HashMap<>(4);
         json.put("number", identityId);
         json.put("template_id", templateId);
@@ -90,7 +116,7 @@ public class PushService{
                 .put("keyword3", RepairItemStateEnum.getDescription(pushInfo.getState()))
                 .put("keyword4", pushInfo.getProblem())
                 .put("keyword5", DateUtil.format(new Date(pushInfo.getCreateTime()*1000), "yyyy-MM-dd HH:mm:ss"))
-                .put("remark", "请尽快处理").build());
+                .put("remark", remark).build());
         json.put("miniprogram", ImmutableMap.<String, String>builder()
                 .put("appid", WxMaConfiguration.getAppid())
                 .put("pagepath", "pages/index/manage/order/orderDetails/orderDetails?orderId="
