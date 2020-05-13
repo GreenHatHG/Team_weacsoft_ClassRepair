0611899c97e829e6be87d7b33e5d685861a7e88e
GreenHatHG <1239776759>
2020-05-13 15:18:49 +0800

    缺陷-订单处理-fixed

--- classrepair-repair-item/src/main/java/team/weacsoft/repair/service/impl/CancelReceiveServiceImpl.java
+++ classrepair-repair-item/src/main/java/team/weacsoft/repair/service/impl/CancelReceiveServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import team.weacsoft.common.consts.RepairItemStateEnum;
 import team.weacsoft.common.exception.BadRequestException;
 import team.weacsoft.common.exception.EntityNotFoundException;
-import team.weacsoft.common.exception.handler.GlobalExceptionHandler;
 import team.weacsoft.repair.entity.RepairItem;
 import team.weacsoft.repair.service.BaseUpdateRepairItemService;
 import team.weacsoft.user.entity.UserInfo;
@@ -26,8 +25,9 @@ public class CancelReceiveServiceImpl extends BaseUpdateRepairItemService {
         if(repairItem.getState()==null){
             throw new EntityNotFoundException("RepairIteam的","state","为空");
         }
-        if(!repairItem.getState().equals(RepairItemStateEnum.PROCESSING.getState())){
-            throw new BadRequestException(40088, "该订单未处于处理中状态，目前状态:"+
+        if(!repairItem.getState().equals(RepairItemStateEnum.PROCESSING.getState())
+             || !repairItem.getState().equals(RepairItemStateEnum.PENDING.getState())){
+            throw new BadRequestException(40088, "该订单未处于处理中或者待处理状态，目前状态:"+
                     RepairItemStateEnum.getDescription(repairItem.getState()));
         }
         repairItem.setState(RepairItemStateEnum.PENDING.getState());
